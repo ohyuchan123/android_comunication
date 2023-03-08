33879e39fa2bdecf33982888af659717f0902c57
ohyuchan123 <oyuchan50@gmail.com>
2023-03-09 01:23:15 +0900

    Initial commit

---
+++ src/main/java/com/example/android_comunication/AndroidComunicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.android_comunication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidComunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndroidComunicationApplication.class, args);
+	}
+
+}
